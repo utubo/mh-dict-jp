5f44e1e1399ada14fa0356306e3e6d88ae008e92
utubo <utbutb@gmail.com>
2023-04-30 04:30:25 +0900

    refactor: もっと単純で良かった

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #/usr/local/bin/bash
 mkdir -p .output
-if [ -z "$GITHUB_WORKSPACE" ]; then
-  GITHUB_WORKSPACE=.
-fi
-SRC=$GITHUB_WORKSPACE/dict
-for f in $SRC/*.txt; do
+SRC=./dict/*.txt
+for f in $SRC; do
   OUTFILE=.output/`basename $f`
   grep --no-filename -e '^[^#].*' $f | sort -u > $OUTFILE
   ZIPFILE=`echo $OUTFILE | sed 's/\.txt$/.zip/'`
@@ -12,5 +9,5 @@ for f in $SRC/*.txt; do
   rm -f $OUTFILE
 done
 # all
-grep --no-filename -e '^[^#].*' $SRC/*.txt | sort -u > .output/mh-dict-jp-all.txt
+grep --no-filename -e '^[^#].*' $SRC | sort -u > .output/mh-dict-jp-all.txt
 
